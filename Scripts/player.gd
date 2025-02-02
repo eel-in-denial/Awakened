57d9979c40ee3431bd602efaf02c0f0f21fc2d54
Daniel Lee <danielwlee515@gmail.com>
2025-02-03 08:23:09 +1100

    more customizable movement

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,25 +1,39 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const ACCELERATION := 50.0
+const DECELERATION := 50.0
+const MAX_SPEED := 300.0
+const JUMP_VELOCITY := -550.0
+const gravity := Global.gravity
+var direction := 0.0
 
+func _ready() -> void:
+	pass
+
+func _movement(delta: float) -> void:
+	# if direction inputs detected, add to velocity, if not deccelerate to 0
+	direction = Input.get_axis("left", "right")
+	if direction:
+		velocity.x += direction * ACCELERATION * delta * 60
+		velocity.x = clamp(velocity.x, -MAX_SPEED, MAX_SPEED)
+	elif !direction and velocity.x:
+		velocity.x = move_toward(velocity.x, 0, DECELERATION * delta * 60)
+		
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
-
+		velocity.y += gravity * delta
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("left", "right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	if Input.is_action_just_released("jump") and velocity.y < 0:
+		velocity.y = 100
+	
+	
+	_movement(delta)
+	
+	
+	#does godot physics stuff
 	move_and_slide()
