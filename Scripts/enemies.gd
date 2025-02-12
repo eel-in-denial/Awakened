00bf531204cced5ebecf244246ef51f73bcbe27c
123esd <maxan6592742@outlook.com>
2025-02-12 21:05:18 +1100

    Enemy
    woooooooooo the enemy is done! other than the janky knockback

--- Scripts/enemies.gd
+++ Scripts/enemies.gd
@@ -3,22 +3,42 @@ class_name Enemy
 
 const SPEED = 150.0
 var direction := 1
-#var health = 3:
-	#set(value): if health 
+var health = 3
+@onready var hitbox: Area2D = $Hitbox
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# Apply gravity if not on the floor
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	if is_on_wall():
-		direction *= -1
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	velocity.x = direction * SPEED
 	move_and_slide()
+
+	# Check for collisions
+	for i in range(get_slide_collision_count()):
+		var collision = get_slide_collision(i)
+		var body = collision.get_collider()
+		
+		if body is Player:
+			body._deal_damage_to_player(1, global_position)
+		elif body.is_in_group("Walls"):
+			direction *= -1
+	
+func _on_enemy_body_contact(body: Node2D) -> void:
+	if body is Player:
+		body._deal_damage_to_player(1, global_position)
+	
+	
+func _on_hitbox_body_entered(body: Node2D) -> void:
+	if body is Player:
+		body._deal_damage_to_player(1, global_position)
+	
+func _deal_damage(damage: int) -> void:
+	health -= damage
+	if health <= 0:
+		_die()
+
+func _die() -> void:
+		queue_free()
+	
 	
