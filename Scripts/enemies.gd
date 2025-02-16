416b796292c38f3173d0215819b03ac6634ab0e8
123esd <maxan6592742@outlook.com>
2025-02-16 13:33:19 +1100

    enemy updates

--- Scripts/enemies.gd
+++ Scripts/enemies.gd
@@ -5,6 +5,10 @@ const SPEED = 150.0
 var direction := 1
 var health = 3
 @onready var hitbox: Area2D = $Hitbox
+@onready var ledge_detector: RayCast2D = $LedgeDetector
+
+func _ready() -> void:
+	ledge_detector.enabled = true
 
 func _physics_process(delta: float) -> void:
 	# Apply gravity if not on the floor
@@ -14,6 +18,10 @@ func _physics_process(delta: float) -> void:
 	velocity.x = direction * SPEED
 	move_and_slide()
 
+	if not ledge_detector.is_colliding():
+		direction *= -1
+		await get_tree().create_timer(1).timeout
+		
 	# Check for collisions
 	for i in range(get_slide_collision_count()):
 		var collision = get_slide_collision(i)
@@ -27,7 +35,6 @@ func _physics_process(delta: float) -> void:
 func _on_enemy_body_contact(body: Node2D) -> void:
 	if body is Player:
 		body._deal_damage_to_player(1, global_position)
-	
 	
 func _on_hitbox_body_entered(body: Node2D) -> void:
 	if body is Player:
