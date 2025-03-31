f005d93511b1840562af47833edc35401734fdeb
Daniel Lee <danielwlee515@gmail.com>
2025-03-31 20:59:14 +1100

    editing

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -4,15 +4,21 @@ class_name Main extends Node2D
 @onready var player = $Player
 var adjacent_rooms: Dictionary[Area2D, Node2D]
 
-var current_room = load("res://Scenes/1_Rooms/first_room.tscn").instantiate()
+@export var first_room: PackedScene
+var current_room: Node2D
 var prev_room: Node2D
+var debug_door: Area2D
 
 func _ready() -> void:
+	current_room = first_room.instantiate()
 	remove_child(player)
 	Global.main = self
 	room.add_child(current_room)
 	load_rooms(current_room.get_node("Doors").adjacent_rooms) 
-	set_player_pos(current_room.get_node("Player Spawn").global_position)
+	if current_room.has_node("Player Spawn"):
+		set_player_pos(current_room.get_node("Player Spawn").global_position)
+	else:
+		set_player_pos(debug_door.get_node("Marker2D").global_position)
 	current_room.get_node("Camera Bound").set_camera_bounds()
 	
 
@@ -38,4 +44,5 @@ func set_player_pos(position):
 func load_rooms(room_paths: Dictionary[Area2D, String]):
 	for room in room_paths:
 		adjacent_rooms[room] = load(room_paths[room]).instantiate()
+		debug_door = room
 	
